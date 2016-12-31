ae647ed0731d20fb6aaa1da595ce7c203ea11aef
Serp4 <bartosz.kusiak95@gmail.com>
2016-12-31 03:48:04 +0100

    Dodano pośrednie menu ( bez dialoga) - zestaw

---
+++ app/src/main/java/com/example/bartek/projekt2016/GRA/Zestaw.java
@@ -0,0 +1,13 @@
+package com.example.bartek.projekt2016.GRA;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Zestaw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zestaw);
+    }
+}
